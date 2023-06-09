9fe918a477ade27800c904c22ab805d7183a699b
Ritik Sharma <63917915+sharmaritik1703@users.noreply.github.com>
2023-06-10 00:22:01 +0530

    Update Weather-Forecast.py

--- Weather-Forecast.py
+++ Weather-Forecast.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 
 st.title("Weather Forecast")
-st.markdown("A machine learning algorithm to predict the weather status using entered parameters.")
+st.subheader("A machine learning algorithm to predict the weather status using entered parameters.")
 
 st.subheader("Accepted Parameters")
 df = pd.read_csv("weather_test.csv")
 df.drop("Unnamed: 0", axis=1, inplace=True)
 
-st.table(data=df.columns)
+st.dataframe(data=df.columns, width=300)
