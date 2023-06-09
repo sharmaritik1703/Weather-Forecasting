06a38555e5d86da4c6e63e18d44cdc7b0b33a7a2
Ritik Sharma <63917915+sharmaritik1703@users.noreply.github.com>
2023-06-10 00:27:45 +0530

    Update Weather-Forecast.py

--- Weather-Forecast.py
+++ Weather-Forecast.py
@@ -4,9 +4,8 @@ import pandas as pd
 st.title("Weather Forecasting")
 st.markdown("A machine learning algorithm to predict the weather status using entered parameters.")
 
-st.subheader("Accepted Parameters")
 df = pd.read_csv("weather_test.csv")
 df.drop("Unnamed: 0", axis=1, inplace=True)
 
-with st.expander(label=""):
+with st.expander(label="Accepted Parameters"):
   st.dataframe(data=df.columns, width=300, height=450)
