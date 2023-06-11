4de665ec96dbf2d0355423eea1e02638a3a1bd0f
Ritik Sharma <63917915+sharmaritik1703@users.noreply.github.com>
2023-06-11 17:56:49 +0530

    Update Weather-Forecast.py

--- Weather-Forecast.py
+++ Weather-Forecast.py
@@ -6,8 +6,8 @@ st.markdown("""
 $\\textrm{A machine learning algorithm to predict the weather status using entered parameters}$.
 
 - $\\textrm{Go to top left corner}$.
-- $\\textrm{Enter the value of features in **Data** page}$.
-- $\\textrm{See the weather status in **Results** page}$.
+- $\\textrm{Enter the value of features in Data page}$.
+- $\\textrm{See the weather status in Results page}$.
 """)
 
 df = pd.read_csv("weather_test.csv")
