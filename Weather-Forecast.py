ecb19733a8b302ba6a9ad9bf6431dc309cfeae19
Ritik Sharma <63917915+sharmaritik1703@users.noreply.github.com>
2023-06-11 17:53:10 +0530

    Update Weather-Forecast.py

--- Weather-Forecast.py
+++ Weather-Forecast.py
@@ -2,11 +2,16 @@ import streamlit as st
 import pandas as pd
 
 st.title("Weather Forecasting")
-st.markdown("A machine learning algorithm to predict the weather status using entered parameters.")
+st.markdown("""
+A machine learning algorithm to predict the weather status using entered parameters.
+
+- Enter the value of features in **Data** page.
+- See the weather status in **Results** page.
+""")
 
 df = pd.read_csv("weather_test.csv")
 df.drop(["Unnamed: 0", "date"], axis=1, inplace=True)
 
 st.subheader("Features")
-with st.expander(label="👇👇 "):
+with st.expander(label="The accepted features are given here: 👇👇 "):
   st.dataframe(data=df.columns, width=250, height=380)
