087259425cac42d0bd15fef6db9d5ff27ae88046
Ritik Sharma <63917915+sharmaritik1703@users.noreply.github.com>
2023-06-15 19:33:51 +0530

    Update Data.py

--- pages/Data.py
+++ pages/Data.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import pickle
 
 st.set_page_config(page_icon="⚙️")
-st.title("Feature Selection")
+
+st.title("Weather Forecasting")
+st.markdown("A machine learning algorithm to predict the weather status using entered parameters.")
+
+model = pickle.load(open('weather.pkl', 'rb'))
 directions = {'E': 0, 'W': 1, 'N': 2, 'S': 3, 'NE': 4, 'SE': 5, 'NW': 6, 'SW': 7}
 
 if 'data' not in st.session_state:
@@ -15,42 +20,41 @@ col7, col8 = st.columns(2)
 col9, col10 = st.columns(2)
 
 with col1:
-    st.session_state.data[0, 0] = st.slider(label="Average Temperature", min_value=-3.7, max_value=30.0)
+    st.session_state.data[0, 0] = st.slider(label="Average Temperature", min_value=-8.0, max_value=30.0)
 
 with col2:
-    st.session_state.data[0, 1] = st.slider(label="Max. Temperature", min_value=0.0, max_value=37.3)
+    st.session_state.data[0, 1] = st.slider(label="Max. Temperature", min_value=-5.0, max_value=40.0)
 
 with col3:
-    st.session_state.data[0, 2] = st.slider(label="Min. Temperature", min_value=-7.0, max_value=24.0)
+    st.session_state.data[0, 2] = st.slider(label="Min. Temperature", min_value=-13.0, max_value=24.5)
 
 with col4:
-    st.session_state.data[0, 3] = st.slider(label="Precipitation", min_value=0.0, max_value=43.0)
+    st.session_state.data[0, 3] = st.slider(label="Precipitation", min_value=0.0, max_value=52.06)
 
 with col5:
-    st.session_state.data[0, 4] = st.slider(label="Avg. Wind Speed", min_value=1.0, max_value=5.2)
+    st.session_state.data[0, 4] = st.slider(label="Avg. Wind Speed", min_value=0.6, max_value=4.6)
 
 with col6:
-    st.session_state.data[0, 5] = st.slider(label="Max. Wind Speed", min_value=2.0, max_value=13.3)
+    st.session_state.data[0, 5] = st.slider(label="Max. Wind Speed", min_value=0.8, max_value=12.7)
 
 with col7:
     wsd = st.selectbox(label="Max. Wind Speed Direction", options=directions.keys())
     st.session_state.data[0, 6] = directions[wsd]
 
 with col8:
-    st.session_state.data[0, 7] = st.slider(label="Max. Inst. Wind Speed ", min_value=2.0, max_value=21.5)
+    st.session_state.data[0, 7] = st.slider(label="Max. Inst. Wind Speed ", min_value=1.8, max_value=21.5)
 
 with col9:
     msd = st.selectbox(label="Max. Inst. Wind Speed Direction", options=directions.keys())
     st.session_state.data[0, 8] = directions[msd]
 
 with col10:
-    st.session_state.data[0, 9] = st.slider(label="Min. Atmospheric Pressure", min_value=1000.00, max_value=1026.00)
+    st.session_state.data[0, 9] = st.slider(label="Min. Atmospheric Pressure", min_value=1009.00, max_value=1022.00)
 
 main = pd.read_csv("weather_test.csv")
 df = pd.DataFrame(data=st.session_state.data, columns=main.columns[2:])
 
 def save():
     df.to_csv("user-data.csv")
-
 
 st.button(label="Save", on_click=save)
