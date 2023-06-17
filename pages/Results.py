7fb1dc40b5e584ef225b12468a8ebe822124d7f0
Ritik Sharma <63917915+sharmaritik1703@users.noreply.github.com>
2023-06-17 15:44:29 +0530

    Update Results.py

--- pages/Results.py
+++ pages/Results.py
@@ -9,7 +9,7 @@ image1 = Image.open("sunny.jpg")
 image2 = Image.open("cloudy.jpg")
 image3 = Image.open("rainy.jpg")
 
-df = pd.read_csv("user_data.csv")
+df = pd.read_csv("user-data.csv")
 df.drop('Unnamed: 0', axis=1, inplace=True)
 
 st.title("See Weather: ")
