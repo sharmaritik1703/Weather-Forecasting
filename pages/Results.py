608b0561985d0fd48bb0c27e094d6e4018949460
Ritik Sharma <63917915+sharmaritik1703@users.noreply.github.com>
2023-06-17 15:43:31 +0530

    Update Results.py

--- pages/Results.py
+++ pages/Results.py
@@ -2,13 +2,15 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 import pickle
-from Data import df
 
 model = pickle.load(open('model.pkl', 'rb'))
 
 image1 = Image.open("sunny.jpg")
 image2 = Image.open("cloudy.jpg")
 image3 = Image.open("rainy.jpg")
+
+df = pd.read_csv("user_data.csv")
+df.drop('Unnamed: 0', axis=1, inplace=True)
 
 st.title("See Weather: ")
 st.markdown("Click on **Forecast** to see weather.")
