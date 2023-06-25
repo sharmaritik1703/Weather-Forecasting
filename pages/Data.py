6d4380d8f3a9c83c8fcd7041f6c6abdd73be27fc
Ritik Sharma <63917915+sharmaritik1703@users.noreply.github.com>
2023-06-25 14:34:24 +0530

    Update Data.py

--- pages/Data.py
+++ pages/Data.py
@@ -16,22 +16,22 @@ col7, col8 = st.columns(2)
 col9, col10 = st.columns(2)
 
 with col1:
-    st.session_state.data[0, 0] = st.slider(label="Average Temperature", min_value=-8.00, max_value=30.00)
+    st.session_state.data[0, 0] = st.slider(label="Average Temperature", min_value=-8.00, max_value=29.5)
 
 with col2:
-    st.session_state.data[0, 1] = st.slider(label="Max. Temperature", min_value=-5.00, max_value=40.00)
+    st.session_state.data[0, 1] = st.slider(label="Max. Temperature", min_value=0.50, max_value=40.00)
 
 with col3:
     st.session_state.data[0, 2] = st.slider(label="Min. Temperature", min_value=-13.00, max_value=24.50)
 
 with col4:
-    st.session_state.data[0, 3] = st.slider(label="Precipitation", min_value=0.00, max_value=166.00)
+    st.session_state.data[0, 3] = st.slider(label="Precipitation", min_value=0.00, max_value=84.00)
 
 with col5:
-    st.session_state.data[0, 4] = st.slider(label="Avg. Wind Speed", min_value=0.10, max_value=6.00)
+    st.session_state.data[0, 4] = st.slider(label="Avg. Wind Speed", min_value=0.30, max_value=5.0)
 
 with col6:
-    st.session_state.data[0, 5] = st.slider(label="Max. Wind Speed", min_value=0.80, max_value=12.70)
+    st.session_state.data[0, 5] = st.slider(label="Max. Wind Speed", min_value=1.00, max_value=11.00)
 
 with col7:
     wsd = st.selectbox(label="Max. Wind Speed Direction", options=directions.keys())
@@ -45,7 +45,7 @@ with col9:
     st.session_state.data[0, 8] = directions[msd]
 
 with col10:
-    st.session_state.data[0, 9] = st.slider(label="Min. Atmospheric Pressure", min_value=1009.00, max_value=1030.00)
+    st.session_state.data[0, 9] = st.slider(label="Min. Atmospheric Pressure", min_value=999.00, max_value=1022.00)
 
 main = pd.read_csv("weather_test.csv")
 df = pd.DataFrame(data=st.session_state.data, columns=main.columns[2:])
