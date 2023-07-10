3a0629f2dde31a2f79fb121b4c55eb0f48e51ce9
Ritik Sharma <63917915+sharmaritik1703@users.noreply.github.com>
2023-07-10 21:56:25 +0530

    Update Weather-Forecast.py

--- Weather-Forecast.py
+++ Weather-Forecast.py
@@ -5,6 +5,28 @@ st.title("Weather Forecasting")
 st.markdown("""
 A machine learning algorithm to predict the weather status using entered parameters.
 
-- $\\textrm{Go to top left corner}$.
-- $\\textrm{Enter the value of features in Status page}$.
+`Precipation Type --- Rain or Snow
+
+`Temperature (C)` --- Tempearture in Celcius
+
+`Humidity`
+
+`Wind Speed (km/h)`
+
+`Wind Bearing (degrees)`
+
+`Visibility (km)`
+
+`Pressure (millibars)`
 """)
+
+st.divider()
+
+st.header("Steps")
+st.markdown("""
+- Go to top left corner
+- Click on **Status** Page.
+- Enter the Data.
+""")
+
+
