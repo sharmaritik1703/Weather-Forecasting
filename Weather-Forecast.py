a1540fa02e5c1a84a73df3b55f7aa416180e2ca4
Ritik Sharma <63917915+sharmaritik1703@users.noreply.github.com>
2023-07-10 21:26:55 +0530

    Update Weather-Forecast.py

--- Weather-Forecast.py
+++ Weather-Forecast.py
@@ -6,13 +6,5 @@ st.markdown("""
 A machine learning algorithm to predict the weather status using entered parameters.
 
 - $\\textrm{Go to top left corner}$.
-- $\\textrm{Enter the value of features in Data page}$.
-- $\\textrm{See the weather status in Results page}$.
+- $\\textrm{Enter the value of features in Status page}$.
 """)
-
-df = pd.read_csv("weather_test.csv")
-df.drop(["Unnamed: 0", "date"], axis=1, inplace=True)
-
-st.subheader("Features")
-with st.expander(label="The accepted features are given here: 👇👇 "):
-  st.dataframe(data=df.columns, width=250, height=380)
