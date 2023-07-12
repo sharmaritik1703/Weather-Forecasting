7772bc59166df9c1df92c6f2f15b8b30a2ed8819
Ritik Sharma <63917915+sharmaritik1703@users.noreply.github.com>
2023-07-12 14:11:02 +0530

    Update Status.py

--- pages/Status.py
+++ pages/Status.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import pickle
 
-model = pickle.load(open("Model.h5", 'rb'))
+model = pickle.load(open("Model.pkl", 'rb'))
 
 st.title("Weather Forecasting System")
 
